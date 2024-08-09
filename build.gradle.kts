65398fe58b260fac23c12e0782571a35a6251323
Herbert Smith <smith.11429@buckeyemail.osu.edu>
2024-08-09 00:20:29 -0500

    Fixed publishing issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 
 plugins {
+    id("maven-publish")
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.10"
 }
 
-group = "example.com"
-version = "0.0.1"
+group = "com.iideprived.rain"
+version = "1.0.0"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
@@ -32,4 +33,25 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.21") // Reflections
     implementation("io.github.classgraph:classgraph:4.8.162")
 
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/iideprived/p/rain/maven")
+        }
+    }
+}
+
+tasks.named("publishToMavenLocal"){
+    dependsOn(tasks.named("assemble"))
 }
